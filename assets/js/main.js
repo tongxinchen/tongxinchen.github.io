a08026b6b649609637b85e7973ced7e03d6d1db4
Tongxin Chen <43999211+tongxinchen@users.noreply.github.com>
2023-12-09 13:28:12 +0000

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,21 @@
 // Select all the containers
 let containers = document.querySelectorAll('.container');
 
-// Manually set the href attribute for each container
-containers[0].querySelector('p').setAttribute('href', 'https://tongxinchen.github.io/publication#p_fig1');
-containers[1].querySelector('p').setAttribute('href', 'https://tongxinchen.github.io/publication#p_fig2');
-containers[2].querySelector('p').setAttribute('href', 'https://tongxinchen.github.io/publication#p_fig3');
-containers[3].querySelector('p').setAttribute('href', 'https://tongxinchen.github.io/publication#p_fig4');
+// Loop through each container
+containers.forEach((container, index) => {
+    // Select the p element within the container
+    let pElement = container.querySelector('p');
 
+    // Create a new a element
+    let aElement = document.createElement('a');
+
+    // Set the href attribute of the a element to link to the corresponding position in the publication page
+    aElement.setAttribute('href', `https://tongxinchen.github.io/publication#p_fig${0 + 1}`);
+
+    // Set the content of the a element to be the content of the p element
+    aElement.textContent = pElement.textContent;
+
+    // Replace the p element with the a element in the container
+    container.replaceChild(aElement, pElement);
+});
 console.log("main.js has been executed");
