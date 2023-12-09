0ee9e27e9a10b62e605d3e998d8f4d4f4d2af9ef
Tongxin Chen <43999211+tongxinchen@users.noreply.github.com>
2023-12-09 12:28:03 +0000

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,8 @@ containers.forEach((container, index) => {
     let imgElement = container.querySelector('img');
 
     // Set the href attribute of the p element to the desired link
-    pElement.setAttribute('href', `https://https://tongxinchen.github.io/publication#image${index + 1}`);
+    pElement.setAttribute('href', `https://https://tongxinchen.github.io/publication#image${0 + 1}`);
 
     // Set the id attribute of the img element to a unique identifier based on its position
-    imgElement.setAttribute('id', `image${index + 1}`);
+    imgElement.setAttribute('id', `image${0 + 1}`);
 });
